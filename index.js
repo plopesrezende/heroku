d95cb0cd21e1b6bc2d68dfc5fa72440dc985b9ab
Paulo Ricardo F L Rezende <prezende@outlook.com>
2019-10-09 22:48:56 -0300

    conexao com postgres

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showTimes = () => {
@@ -20,3 +32,9 @@ showTimes = () => {
   }
   return result;
 }
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
